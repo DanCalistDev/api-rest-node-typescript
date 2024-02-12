2502942db9ab3211f387136e8d9c6d7f3f811b49
Dancalist <dancalist.dev@gmail.com>
2024-02-12 12:48:19 -0300

    feat: :sparkles: add code styles configs

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 import {server} from './server/Server';
 
-server.listen(8080, ()=> console.log('App rodando!'))
+server.listen(8080, ()=> console.log('App rodando!'));
--- src/server/Server.ts
+++ src/server/Server.ts
@@ -2,11 +2,8 @@ import express from 'express';
 
 const server = express();
 
+server.get('/', (req, res) => {
+  return res.send('Iniciando Projeto!');
+});
 
-server.get('/', (req, res)=> {
-    return res.send("Iniciando Projeto!")
-})
-
-
-
-export {server};
+export { server };
